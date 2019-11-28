06c1f22ad7470f4e88ea5589d39ecde268f1076f
lolipirate <lolipirate1@gmail.com>
2019-11-28 12:01:35 +0100

    fixed unit test for groups

--- test/cinema/GroupNGTest.java
+++ test/cinema/GroupNGTest.java
@@ -6,9 +6,7 @@
 package cinema;
 
 import static cinema.Cinema.Query;
-import java.sql.ResultSet;
 import java.util.LinkedList;
-import java.util.function.BiConsumer;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,131 +17,125 @@ import org.testng.annotations.Test;
  * @author Piratica
  */
 public class GroupNGTest {
-    
+
     public GroupNGTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        
+
         Group group = new Group();
-        
+
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
-        
-       Group group = null;
+
+        Group group = null;
     }
 
     /**
-     * Test of AddGroup method, of class Group.
-     * The method will also test the AddUserToGroup method. 
-     * It will create a group and a super to add to said group. It will then 
-     * update the database with these entries and finally attempt to pull them
-     * for the database to compare with the expected values. 
+     * Test of AddGroup method, of class Group. The method will also test the
+     * AddUserToGroup method. It will create a group and a super to add to said
+     * group. It will then update the database with these entries and finally
+     * attempt to pull them for the database to compare with the expected
+     * values.
      */
     @Test
     public void testAddGroup() {
         System.out.println("AddGroup");
         Group instance = new Group();
-        Group expected = new Group();
+        Group expected;
         User user = new User();
         User expected_user = new User();
-        user.email = "unit@test.ok";
-        user.name = "unit test";
+        user.email = "unit@testing.dk";
+        user.name = "unit test addmember";
         user.phone = 1234;
         user.privilege = 1;
-        instance.name = "unit test";
+        instance.name = "testing groups";
         instance.onCallSuper = user;
-        
-        
+
         user.AddUser("test");
-        
-        
-        
+
         user.uId = Cinema.GetUser(user.email).uId;
-        
+
         instance.AddGroup();
-        
+
         expected = Cinema.GetGroup(instance.name);
-        
+
         Assert.assertEquals(instance.name, expected.name);
-        
+
         Assert.assertEquals(user.email, expected.onCallSuper.email);
-        
-        RemoveGroupmemberFromDB(instance.name);
+
+        RemoveGroupmembersFromDB(instance.name);
         RemoveGroupFromDB(instance.name);
         UserNGTest.RemoveFromDB(user.email);
     }
-    
+
     /**
      * Test of AddUserToGroup method, of class Group.
      */
     @Test
     public void testAddUserToGroup() {
+
+        System.out.println("AddUserToGroup");
         User superuser = new User();
         User toAdd = new User();
         Group instance = new Group();
-        Group expected_group = new Group();
-        User expected_user = new User();
+        Group expected_group;
+        User expected_user;
 
-        superuser.email = "unit@test.ok";
+        superuser.email = "unit2@test.dk";
         superuser.name = "Unit Test";
         superuser.phone = 1337;
         superuser.privilege = 1;
         superuser.AddUser("unit");
-        
-        toAdd.email = "test@unit.test";
+        superuser.uId = Cinema.GetUser(superuser.email).uId;
+
+        toAdd.email = "unit3@unit.test";
         toAdd.name = "mr Unit Test";
         toAdd.phone = 4321;
         toAdd.privilege = 0;
         toAdd.AddUser("test");
-        
-        instance.name = "Unit Test";
+        toAdd.uId = Cinema.GetUser(toAdd.email).uId;
+
+        instance.name = "test group";
         instance.onCallSuper = superuser;
         instance.AddGroup();
         instance.AddUserToGroup(toAdd);
-        
+
         expected_group = Cinema.GetGroup(instance.name);
-        
-        expected_user = expected_group.Members.getKey();
-        
+
+        expected_user = expected_group.Members.get(1).getKey();
+
         Assert.assertEquals(toAdd.name, expected_user.name);
-        
-        
-        
-        RemoveGroupmemberFromDB(instance.name);
+
+        RemoveGroupmembersFromDB(instance.name);
         RemoveGroupFromDB(instance.name);
         UserNGTest.RemoveFromDB(superuser.email);
         UserNGTest.RemoveFromDB(toAdd.email);
-        
+
     }
-    
-    
+
     /*
     * General cleanup methods to make the tests reusable and avoid
     * cluttering up the database. 
-    */
+     */
     private void RemoveGroupFromDB(String group_name) {
-        
+
         LinkedList vars = new LinkedList();
         vars.add(group_name);
         String query = "DELETE FROM groups WHERE name = ?";
-        
-        BiConsumer<LinkedList, ResultSet> f = (l, rs) -> {};
-        Query(query, vars, f);
+
+        Query(query, vars);
     }
-    
-    private void RemoveGroupmemberFromDB(String name) {
+
+    private void RemoveGroupmembersFromDB(String name) {
         LinkedList vars = new LinkedList();
         vars.add(name);
         String query = "DELETE FROM groupmembers WHERE groupname = ?";
-        
-        BiConsumer<LinkedList, ResultSet> f = (l, rs) -> {};
-        Query(query, vars, f);
+
+        Query(query, vars);
     }
-    
-    
-    
+
 }
